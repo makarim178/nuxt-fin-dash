e7eed4d58572a3e81342613940875026108cf937
Mir Ashiful Karim <MirAshiful.Karim@triosstudent.com>
2025-07-18 13:33:00 -0600

    create api endpoint for retrieving roles

--- server/api/role-setup/index.get.ts
+++ server/api/role-setup/index.get.ts
@@ -1,17 +1,19 @@
 import { getRoles } from "~~/server/supabase/queries/roleSetupQuery"
+import { rolesSelectResponseSchema } from "~~/shared/types";
 
-export default defineEventHandler(async (event) => {
+export default defineEventHandler(async () => {
   try {
     const roles: {id: number; role: string}[] = await getRoles()
     const rolesArr = roles.reduce((acc: string[], { role }) => {
       acc.push(role)
       return acc
     } , [])
-    return {
+    return rolesSelectResponseSchema.parse({
       roles: rolesArr,
       rolesCount: rolesArr.length
-    }
+    })
   } catch (error) {
+    console.log(error)
     if (error instanceof Error) {
             throw createError({
                 statusCode: 400,
--- shared/types/userRoleTypes.ts
+++ shared/types/userRoleTypes.ts
@@ -3,7 +3,7 @@ import { creationFields } from "./commonFields";
 import { relations } from "drizzle-orm";
 import { user } from "./user";
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod'
-import type { z } from "zod/v4";
+import { z } from "zod/v4";
 
 export const userRoleTypes = pgTable('userRoleTypes', {
     id: serial('id').notNull().primaryKey(),
@@ -17,6 +17,13 @@ export const roleTypesRelations = relations(userRoleTypes, ({ one }) => ({
         references: [user.roleTypeId]
     })
 }))
+
+export const rolesSelectResponseSchema = z.object({
+    roles: z.array(z.string()),
+    rolesCount: z.number()
+})
+
+export type RolesSelectResponseSchema = z.output<typeof rolesSelectResponseSchema>
 
 export const userRoleSchema = createSelectSchema(userRoleTypes)
 export type UserRoleSchema = z.infer<typeof userRoleSchema>
